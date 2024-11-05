5dca15c547836123e88ae7f7b9a4f10140a1c913
D Surya Prakash Reddy <125757799+surya8980@users.noreply.github.com>
2024-11-05 21:17:22 +0530

    Add files via upload

---
+++ A2OJ/WordCapitalization.java
@@ -0,0 +1,21 @@
+/*Capitalization is writing a word with its first letter as a capital letter. Your task is to capitalize the given word.
+
+Note, that during capitalization all the letters except the first one remains unchanged.
+
+Input
+A single line contains a non-empty word. This word consists of lowercase and uppercase English letters. 
+The length of the word will not exceed 103.
+
+Output
+Output the given word after capitalization. */
+
+import java.util.Scanner;
+
+public class WordCapitalization {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(str.substring(0, 1).toUpperCase() + str.substring(1));
+        sc.close();
+    }
+}
